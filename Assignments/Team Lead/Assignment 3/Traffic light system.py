a30e1aa7882fdc328198c632f65e3c464d931887
Haashini <83541597+Haashini@users.noreply.github.com>
2022-10-07 23:03:12 +0530

    Create Traffic light system.py

---
+++ Assignments/Team Lead/Assignment 3/Traffic light system.py
@@ -0,0 +1,30 @@
+s (24 sloc)  579 Bytes
+
+loop = 1
+print("TRAFFIC LIGHT SYSTEM")
+print("Input Data Catagorization:")
+print(" RED:STOP")
+print(" YELLOW:GET READY")
+print(" GREEN:GO")
+print("----------------------------")
+loop=eval(input("press 1 to start : "))
+while True:
+ print("enter input signal colour to run the program : ")
+ signal = input()
+ if signal == "red":
+ print("STOP THE VEHICLE FOR NEXT 60 SECONDS")
+ else:
+ if signal =="yellow":
+ print("get ready")
+ else:
+ if signal=="green":
+ print("go")
+ else:
+ if signal=="stop":
+ print("you choose to end the program")
+ else:
+ print("enter only the correct signal")
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
